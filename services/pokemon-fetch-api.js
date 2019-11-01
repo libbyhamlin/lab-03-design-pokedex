0b2fc5121d0b33879ed6c018039c497e52e86001
Libby Hamlin <bravolibby@gmail.com>
2019-11-01 11:44:42 -0700

    finishing up lab. nav isn't responsive yet.

---
+++ services/pokemon-fetch-api.js
@@ -0,0 +1,7 @@
+const URL =
+  'https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=char&defense=50';
+
+export async function getMovies() {
+    const response = await fetch(URL);
+    return await response.json();
+}
